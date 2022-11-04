769191a2d7843503279ec764c3084dd27798c193
Srikanth Saindla <110750154+SrikanthSaindla@users.noreply.github.com>
2022-11-04 15:25:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react'
+import "./App.css"
+ 
 
-function App() {
+const App = () => {
+const[hr,setHr]=useState()
+const[min,setMin]=useState()
+const[sec,setSec]=useState()
+  useEffect(()=>{
+     
+     let interval= setInterval(() => {
+        let clock=new Date()
+      let hr=clock.getHours()
+      let min=clock.getMinutes()
+      let sec=clock.getSeconds()
+
+      let cal_hr=(hr*30)+(min/2)
+      setHr(cal_hr)
+      let cal_min=(min*6)+(sec/10)
+      setMin(cal_min)
+      let cal_sec=(sec*6)
+      setSec(cal_sec)
+
+      return () => clearInterval(interval);
+    },[]);
+     
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='clock'>
+      <div className='hour hand'
+      style={{transform:`rotate(${hr}deg)`}}></div>
+      <div className='minute hand'
+      style={{transform:`rotate(${min}deg)`}}></div>
+      <div className='second hand'
+      style={{transform:`rotate(${sec}deg)`}}></div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
